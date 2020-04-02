5a5a888a26ba7dbb8266155b88efba97e9341195
seaXll <2252593269@qq.com>
2020-04-02 23:04:06 +0800

    1.完成 github 登录认证 2.并在 README.md 中记录 github 三方登录的步骤

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 ## Community -- build by java
 
+### github 登录权限：
+1. 引导用户跳转 github 授权登录页面 https://github.com/login/oauth/authorize，参数如下
+    - client_id：必填，注册 OAuth App 时从 GitHub 得到的 client_id
+    - redirect_uri：授权后将用户发送到应用程序中的URL，即 注册 OAuth 时填写的 Authorization callback URL
+    - login：建议用于登录和授权应用程序的特定帐户
+    - scope：授权范围
+    - state：不可猜测的随机字符串。它用于防止跨站点请求伪造攻击
+    - allow_signup：在OAuth流程期间，是否向未认证的用户提供注册GitHub的选项。默认值为true。false当策略禁止注册时使用
+2. 用户授权后重定向到 redirect_uri，返回code 参数值，再通过code 再次访问 github：https://github.com/login/oauth/access_token
+    - client_id：必填
+    - client_secret：必填，注册 OAuth App 时从 GitHub 得到的 client_secret
+    - code：必填，收到的作为对步骤1的响应的代码
+    - redirect_uri
+    - state
+    
+    默认情况下，github 响应形式为：
+    ```
+   access_token=e72e16c7e42f292c6912e7710c838347ae178b4a&token_type=bearer
+    ```
+   还可以根据 Accept Header 以不同的格式接收内容
+   ```
+   Accept: application/json
+   {"access_token":"e72e16c7e42f292c6912e7710c838347ae178b4a", "scope":"repo,gist", "token_type":"bearer"}
+   
+   Accept: application/xml
+   <OAuth>
+     <token_type>bearer</token_type>
+     <scope>repo,gist</scope>
+     <access_token>e72e16c7e42f292c6912e7710c838347ae178b4a</access_token>
+   </OAuth>
+   ```
+3. 通过获取的access_token 换取 用户信息: https://api.github.com/user，参数：
+    - access_token：第二步得到的 access_token 令牌
 ### 资料
 [Github OAuth](https://developer.github.com/apps/building-github-apps/)
--- src/main/java/com/seaxll/community/cotroller/AuthorizeController.java
+++ src/main/java/com/seaxll/community/cotroller/AuthorizeController.java
@@ -1,5 +1,10 @@
 package com.seaxll.community.cotroller;
 
+import com.seaxll.community.dto.AccessTokenDTO;
+import com.seaxll.community.dto.GithubUser;
+import com.seaxll.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,11 +19,30 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(
             @RequestParam(name = "code") String code,
-            @RequestParam(name = "state") String state) {
-
+            @RequestParam(name = "state", required = false, defaultValue = "1") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
         return "index";
     }
 }
