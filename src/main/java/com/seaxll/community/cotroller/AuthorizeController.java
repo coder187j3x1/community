9a46b48347adcf012d28126c24c38e9402e5bac4
seaXll <2252593269@qq.com>
2020-04-03 22:48:18 +0800

    1.初步实现登录前菜单栏显示登录；登录后显示用户name 目前问题：登录成功过的游览器再次进去首页，还是显示登录，并且点击登录在GitHubProvider 的 getUser() 方法中请求https://api.github.com/user?access_token=时会出现java.net.SocketException: Connection reset的异常

--- src/main/java/com/seaxll/community/cotroller/AuthorizeController.java
+++ src/main/java/com/seaxll/community/cotroller/AuthorizeController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 /**
  * ClassName: AuthorizeController
  * Description:
@@ -33,7 +36,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(
             @RequestParam(name = "code") String code,
-            @RequestParam(name = "state", required = false, defaultValue = "1") String state) {
+            @RequestParam(name = "state", required = false, defaultValue = "1") String state,
+            HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -42,7 +46,14 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if (user != null) {
+            // 登录成功，写cookie 和 session
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+            return "redirect:/";
+        } else {
+            // 登录失败
+            return "redirect:/";
+        }
     }
 }
