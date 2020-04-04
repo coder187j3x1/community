a9d088355f692f39bd86f45c45fd9276828e1d8d
seaXll <2252593269@qq.com>
2020-04-04 21:37:01 +0800

    1.modify PublishController.java 接收提交问题表单的post请求，并插入数据

--- src/main/java/com/seaxll/community/cotroller/PublishController.java
+++ src/main/java/com/seaxll/community/cotroller/PublishController.java
@@ -1,7 +1,19 @@
 package com.seaxll.community.cotroller;
 
+import com.seaxll.community.mapper.QuestionMapper;
+import com.seaxll.community.mapper.UserMapper;
+import com.seaxll.community.model.Question;
+import com.seaxll.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.sql.Timestamp;
 
 /**
  * ClassName: PublishController
@@ -14,8 +26,67 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PublishController {
 
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
+    UserMapper userMapper;
+
     @GetMapping("/publish")
     public String publish() {
         return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String doPublish(
+            @RequestParam(value = "title", required = false) String title,
+            @RequestParam(value = "description", required = false) String description,
+            @RequestParam(value = "tag", required = false) String tag,
+            HttpServletRequest request,
+            Model model) {
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+        model.addAttribute("tag", tag);
+        if ("".equals(title)) {
+            model.addAttribute("error", "标题不能为空");
+            return "publish";
+        }
+        if ("".equals(description)) {
+            model.addAttribute("error", "描述不能为空");
+            return "publish";
+        }
+        if ("".equals(tag)) {
+            model.addAttribute("error", "标签不能为空");
+            return "publish";
+        }
+
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setGmtCreate(new Timestamp(System.currentTimeMillis()));
+        question.setGmtModify(question.getGmtCreate());
+
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if ("token".equals(cookie.getName())){
+                String token = cookie.getValue();
+                user = userMapper.finByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+
+        if (user == null) {
+            model.addAttribute("error", "用户未登录");
+            return "publish";
+        }
+
+        question.setCreatorId(user.getId());
+        questionMapper.insertQuestion(question);
+        return "redirect:/";
     }
 }
