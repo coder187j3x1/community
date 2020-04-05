031e46a9ba7231707d882dcd66a6c6c4bd5b6433
seaXll <2252593269@qq.com>
2020-04-05 23:27:41 +0800

    1.add WebConfig.java SessionInterceptor.java 一个拦截器的实现

---
+++ src/main/java/com/seaxll/community/interceptor/SessionInterceptor.java
@@ -0,0 +1,40 @@
+package com.seaxll.community.interceptor;
+
+import com.seaxll.community.provider.UserProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * ClassName: SessionInterceptor
+ * Description:
+ * date: 2020/4/5 22:53
+ *
+ * @author seaxll
+ * @since JDK 1.8
+ */
+@Service
+public class SessionInterceptor implements HandlerInterceptor {
+    @Autowired
+    private UserProvider userProvider;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        userProvider.userCookieVerify(request);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
---
+++ src/main/java/com/seaxll/community/interceptor/WebConfig.java
@@ -0,0 +1,26 @@
+package com.seaxll.community.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * ClassName: WebConfig
+ * Description:
+ * date: 2020/4/5 22:50
+ *
+ * @author seaxll
+ * @since JDK 1.8
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private SessionInterceptor sessionInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(sessionInterceptor).addPathPatterns("/**");
+    }
+}
