f837d39ef9d42a9d0f5c62cf92d32c79e8ebf9a9
seaXll <2252593269@qq.com>
2020-04-05 15:06:00 +0800

    1.add PageDTO.java

---
+++ src/main/java/com/seaxll/community/dto/PageDTO.java
@@ -0,0 +1,25 @@
+package com.seaxll.community.dto;
+
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * ClassName: PageDTO
+ * Description:
+ * date: 2020/4/5 14:56
+ *
+ * @author seaxll
+ * @since JDK 1.8
+ */
+@Data
+public class PageDTO {
+    private List<QuestionDTO> questions;
+    private boolean showPrevious;
+    private boolean showFirstPage;
+    private boolean showNext;
+    private boolean showEndPage;
+    private Integer page;
+    private List<Integer> pages;
+}
